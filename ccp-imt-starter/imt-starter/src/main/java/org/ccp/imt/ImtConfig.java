24c3a56fe1a9829f31f43ad495199cf206788c38
chongpei.chencp <chongpei.chencp@alibaba-inc.com>
2017-05-20 01:17:38 +0800

    ccp imt update

---
+++ ccp-imt-starter/imt-starter/src/main/java/org/ccp/imt/ImtConfig.java
@@ -0,0 +1,10 @@
+package org.ccp.imt;
+
+import org.springframework.beans.factory.annotation.Configurable;
+
+/**
+ * @author 逍冲 [2017年05月18日 下午1:48]
+ */
+@Configurable
+public class ImtConfig {
+}
