f505e1a93992e82aa0a880a623d703dd64171269
chongpei.chencp <chongpei.chencp@alibaba-inc.com>
2017-05-20 01:17:05 +0800

    ccp imt update

---
+++ ccp-imt-starter/imt-starter/src/main/java/org/ccp/imt/annotation/Imt.java
@@ -0,0 +1,45 @@
+package org.ccp.imt.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Imt注解兼容老的interface形式
+ * @author 逍冲
+ *
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Imt {
+	/**
+	 * 方法描述
+	 * @return
+	 */
+	String mehtodDescrption();
+
+	/**
+	 * 适用环境开发，测试，线上
+	 * @return
+	 */
+	String env() default ImtConstant.ENV_PRODUCT;
+
+	/**
+	 * 分组
+	 * @return
+	 */
+	String[] group() default {};
+
+	/**
+	 * 方法参数描述
+	 * @return
+	 */
+	String[] paramDescrption() default {};
+
+	/**
+	 * 方法类型，（只读、读写、Daily只读、Daily读写）
+	 * @return
+	 */
+	public InvokerType type() default InvokerType.READ_ONLY;
+}
