3d30f4325649104871fef250b5622f0e7d60c450
chongpei.chencp <chongpei.chencp@alibaba-inc.com>
2017-05-20 01:17:45 +0800

    ccp imt update

---
+++ ccp-imt-starter/imt-starter/src/main/java/org/ccp/imt/annotation/ImtConstant.java
@@ -0,0 +1,7 @@
+package org.ccp.imt.annotation;
+
+public class ImtConstant {
+	public static final String ENV_PRODUCT = "product";
+	public static final String ENV_DEV = "dev";
+	public static final String ENV_TEST = "test";
+}
