7b9900241c33d324f91661f01a6e3e6c206e5096
chongpei.chencp <chongpei.chencp@alibaba-inc.com>
2017-05-20 01:18:38 +0800

    ccp imt update

---
+++ ccp-imt-starter/imt-starter/src/main/java/org/ccp/imt/annotation/InvokerType.java
@@ -0,0 +1,22 @@
+package org.ccp.imt.annotation;
+
+public enum InvokerType
+{
+    /**
+     * 需要只读权限才能访问；
+     */
+    READ_ONLY,
+    /**
+     * 需要读写权限才能访问；
+     */
+    READ_WRITE,
+    /**
+     * 只在daily环境下可访问，需要只读权限；
+     */
+    DAILY_READ,
+    /**
+     * 只在daily环境下可访问，需要读写权限；
+     */
+    DAILY_WRITE
+}
+
